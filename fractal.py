e275b4f7ab27d4301944846b45c8635fca5b87ae
fbourke <fbourke@users.noreply.github.com>
2016-01-30 15:48:47 -0500

    larger angles

--- fractal.py
+++ fractal.py
@@ -4,22 +4,21 @@ import random
 import sys
 
 def drawLines(start_x,start_y,depth): 
-	if depth > 10:
+	max_depth = 5
+	if depth > max_depth:
 		return
 	x = start_x
 	y = start_y
 	d = depth
 	color = cv2.cv.Scalar(random.randint(0,255), random.randint(0,255), random.randint(0,255));
-	cv2.line(image, (x,y), (x+length/d,y), color, thickness=2, lineType=8, shift=0)
+	cv2.line(image, (x,y), (x+length/d,y), color, thickness=(max_depth-depth)/2, lineType=8, shift=0)
 	color = cv2.cv.Scalar(random.randint(0,255), random.randint(0,255), random.randint(0,255));
-	cv2.line(image, (x,y), (x+length/d,y+deflection/d), color, thickness=2, lineType=8, shift=0)
+	cv2.line(image, (x,y), (x+length/d,y+deflection/d), color, thickness=(max_depth-depth)/2, lineType=8, shift=0)
 	color = cv2.cv.Scalar(random.randint(0,255), random.randint(0,255), random.randint(0,255));
-	cv2.line(image, (x,y), (x+length/d,y-deflection/d), color, thickness=2, lineType=8, shift=0)
+	cv2.line(image, (x,y), (x+length/d,y-deflection/d), color, thickness=(max_depth-depth)/2, lineType=8, shift=0)
 	drawLines(x+length/d, y, d+1)
 	drawLines(x+length/d, y+deflection/d, d+1)
 	drawLines(x+length/d, y-deflection/d, d+1)
-
-
 
 # generate blank image
 width = 800
@@ -27,16 +26,15 @@ height = 600
 image = blank_image = np.zeros((height,width,3), np.uint8)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-
-# make a center dot
-x = 10
+# make a starting dot
+x = width/2
 y = height/2
 cv2.circle(image, (x,y), 2, (0,255,0))
 
 # make some lines
 depth = 1
 length = 200
-deflection = 50
+deflection = 500
 drawLines(x,y,depth)
 
 

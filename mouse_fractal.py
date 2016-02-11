a0ef08fdc3fa03dc3d679666b94862529705a971
fbourke <fbourke@users.noreply.github.com>
2016-02-11 11:39:22 -0500

    add mouse fractal generation--unfortunately, it doesn't always catch the mouse events

--- mouse_fractal.py
+++ mouse_fractal.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 import time
 import sys
+import math
 
 #===================== Function Defs ================================================
 
@@ -36,7 +37,7 @@ def recursiveTree(start_x,start_y,theta,depth):
 	global finish
 	finish = True
 	(new_x, new_y) = drawPolarLine((x,y), length/d, theta)
-	cv2.imshow("Fractal", image)
+	cv2.imshow('image', image)
 	if not finish:
 		key = cv2.waitKey(0)
 	else:
@@ -75,35 +76,32 @@ ix,iy = -1,-1
 
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
-    global ix,iy,drawing,mode
+    global ix,iy,drawing,mode,length
 
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix,iy = x,y
-
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-            if mode == True:
-                cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),2)
-            else:
-                cv2.circle(img,(x,y),5,(0,0,255),-1)
+        print "Caught MouseDown"
 
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
-        if mode == True:
-            cv2.rectangle(img,(ix,iy),(x,y),(0,255,0),2)
-        else:
-            cv2.circle(img,(x,y),5,(0,0,255),-1)
+        length = math.hypot(x-ix, y-iy)
+        angle = np.arctan2(y-iy, x-ix)
+        recursiveTree(ix, iy, angle, 1)
+        print "Caught MouseUp"
 
-img = np.zeros((512,512,3), np.uint8)
+
+image = blank_image = np.zeros((600,800,3), np.uint8)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image',draw_circle)
 
 while(1):
-    cv2.imshow('image',img)
+    cv2.imshow('image',image)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('m'):
         mode = not mode
+    elif k == ord('c'):
+    	image = blank_image
     elif k == 27 or k == ord('q'):
         break
 
